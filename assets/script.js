c7988c9e06aea2c416cd07310caab60fbc0ffccc
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-04-17 18:15:47 -0400

    Added JS function for both focasts with debuging with Tutor Juan

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,75 @@
-
 const APIKey = '951c18a648ee8495defff470c0b80c80';
 const cityInputEl = document.querySelector('#city');
+const buttonEl = document.getElementById('searchBtn');
 
 let city;
 
 const getGeo = async (city) => {
-    const url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + ',US&limit=1&appid=' + APIKey;
-    console.log(url);
+  console.log('this is city = ', city);
+  const url =
+    'http://api.openweathermap.org/geo/1.0/direct?q=' +
+    city +
+    ',US&limit=1&appid=' +
+    APIKey;
+  console.log(url);
 
-    const response = await fetch(url);
-    const result = await response.json();
-    console.log(result[0]);
-    // keys from result
-    console.log(result[0].lat);
-    console.log(result[0].lon);
-    
-    let lat = result[0].lat
-    let lon = result[0].lon
-}
-//    // step 1 - add to storeHistory
-//     function storeHistory(result[0]) {
-//         if (typeof (storage) !== 'undefined') {  
-//         }
-//     }
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+  console.log(result[0]);
+  //   // keys from result
+  console.log(result[0].lat);
+  console.log(result[0].lon);
 
-   // step 2 - lookup daily weather
-//     getToday(lat, lon)
+  getDailyWeather(result[0].lat, result[0].lon);
+};
 
-const getDailyWeather = async function (city) {
-    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIKey}`;
-    const response = await fetch(currentWeatherUrl);
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    }
-  };
+
+const getDailyWeather = async function (lat, lon) {
+  const currentWeatherUrl =
+    'https://api.openweathermap.org/data/2.5/weather?lat=' +
+    lat +
+    '&lon=' +
+    lon +
+    '&appid=' +
+    APIKey;
+  const response = await fetch(currentWeatherUrl);
+  if (response.ok) {
+    const data = await response.json();
+    console.log('This is data = ', data);
+    return data;
+  }
+};
+
+buttonEl.addEventListener('click', function (event) {
+  event.preventDefault();
+  let userCity = document.getElementById('search-input');
+  console.log(userCity.value);
+  getGeo(userCity.value);
+});
+
+//lat 37.1289771&
+//lon  -84.0832646
+
 //     // step 3 - lookup forecast
 //     getForcast(lat, lon)
 
-  const getForcastWeather = async function (city) {
-    const fiveDayForcastUrl = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}`;
-    const response = await fetch(fiveDayForcastUrl);
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    }
-  };
-    
+const getForcastWeather = async function (lat, lon) {
+  const fiveDayForcastUrl =
+    api.openweathermap.org / data / 2.5 / forecast ? lat =' +
+    lat +
+    '&lon=' +
+    lon +
+    '&appid=' +
+     APIKey;
+  const response = await fetch(fiveDayForcastUrl);
+  if (response.ok) {
+    const data = await response.json();
+    console.log('This is data = ', data)
+    return data;
+  }
+};
+
 // EXAMPLE
 // geoPlace = {
 //     "name": "Atlanta",
@@ -56,6 +79,6 @@ const getDailyWeather = async function (city) {
 //     "state": "Georgia"
 // }
 
-    // function storeHistory(geoPlace) {
-    //     // paste local storage append code
-    // }
+// function storeHistory(geoPlace) {
+//     // paste local storage append code
+// }
